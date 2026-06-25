fd8f75739ee7107b18ade4385c33706e97b64d73
= <=>
2026-06-25 15:42:56 +0300

    feat: initial Angularsetup with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50:  '#E1F5EE',
+          100: '#9FE1CB',
+          400: '#1D9E75',
+          600: '#0F6E56',
+          800: '#085041',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
